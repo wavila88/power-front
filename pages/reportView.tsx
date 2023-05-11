cc83346fbe2152cf790c087fd41099c0366b65ba
William Avila <willocoav@example.com>
2023-05-11 17:00:43 -0500

    made investigation of pdf viewer, use div to hide download

--- pages/reportView.tsx
+++ pages/reportView.tsx
@@ -1,34 +1,56 @@
 
 import ContainerComponent from "@/src/components/layouts/containerContent";
 import Footer from "@/src/components/layouts/footer";
-import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
+
 import Head from "next/head";
 import Loading from "@/src/components/loading";
 import PreTitle from "@/src/components/layouts/preTitle";
+import { createReport, report } from "@/src/utils/reportView.utils";
+
+import styles from '../styles/components/reportView.module.css';
+
 
 
 
 const ReportView = () => {
-  const content = 'Asegurate de que todos los detalles esten listos antes de crear tu poder.';
-  const pageNext = '/reportView';
-  const pageBack = '/description';
-  const dispatch = useDispatch();
+  const content = 'Aqui puedes visualizar tu Poder.';
+  const pageNext = '/payment';
+  const pageBack = '/summary';
 
-
+  // Create new plugin instance
+  // document.getElementById('pdf-viewer').addEventListener('contextmenu', function (event) {
+  //   event.preventDefault();
+  // });
 
   return (
     <>
-       <Head>
-        <title>Reporte</title>
+      <Head>
+        <style>
+          {`
+            .pdf-object::-webkit-media-controls-enclosure {
+              display: none !important;
+            }
+          `}
+        </style>
       </Head>
-      <Loading/>
-      <PreTitle content={content} />
-      <ContainerComponent>
-      {/* <Title>Resumen</Title> */}
+      {/* <Document file="http://africau.edu/images/default/sample.pdf">
+        <Page pageNumber={1} />
+      </Document> */}
+  
+  
+      <PreTitle content={content}/>
+      
+      
 
- 
+      <ContainerComponent>
+      <div className={styles.hide}/>
+        <object id="pdf-viewer" data="http://africau.edu/images/default/sample.pdf" type="application/pdf" width="100%" height="100%">
+          <p>Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf">to the PDF!</a></p>
+        </object>
       </ContainerComponent>
-      <Footer back={pageBack} continue={pageNext}/>
+      <Footer nameContinue="Descargar Poder" back={pageBack} continue={pageNext}/>
+
     </>
   )
 }
